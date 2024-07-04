4c8a6ac85e28bc50d808754b3a0e6b949c7d15cd
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 11:54:26 +0200

    Spring

---
+++ LorrettabankApplication.java
@@ -0,0 +1,13 @@
+package com.lorettabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorrettabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorrettabankApplication.class, args);
+	}
+
+}

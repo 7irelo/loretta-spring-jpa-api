624955b3cf460c7e09d3b9fdbd0110c114bd255b
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 12:30:33 +0200

    Create TransactionService.java

---
+++ lorettabank/src/main/java/com/lorettabank/service/TransactionService.java
@@ -0,0 +1,23 @@
+package com.lorettabank.service;
+
+import com.lorettabank.model.Transaction;
+import com.lorettabank.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+}

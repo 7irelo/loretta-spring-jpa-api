7f54b45265b4b39ccd48f1d3e9393b231f13a44c
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 12:39:33 +0200

    Update UserService.java

--- lorettabank/src/main/java/com/lorettabank/service/UserService.java
+++ lorettabank/src/main/java/com/lorettabank/service/UserService.java
@@ -3,11 +3,14 @@ package com.lorettabank.service;
 import com.lorettabank.model.User;
 import com.lorettabank.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -22,5 +25,14 @@ public class UserService {
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
     }
 }

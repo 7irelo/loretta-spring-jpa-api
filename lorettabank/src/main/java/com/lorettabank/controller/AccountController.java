ce01fcca36f543a7059bfa13da36dc16d2535374
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 12:33:32 +0200

    Create AccountController.java

---
+++ lorettabank/src/main/java/com/lorettabank/controller/AccountController.java
@@ -0,0 +1,31 @@
+package com.lorettabank.controller;
+
+import com.lorettabank.model.Account;
+import com.lorettabank.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.saveAccount(account);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+    }
+}

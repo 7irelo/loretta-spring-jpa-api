7b33222868b3dd34cf5c7c8913dd427860858e96
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-05 00:32:54 +0200

    Update User.java

--- lorettabank/src/main/java/com/lorettabank/model/User.java
+++ lorettabank/src/main/java/com/lorettabank/model/User.java
@@ -1,14 +1,25 @@
 package com.lorettabank.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
-    // getters and setters
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Account> accounts;
+
+    // Getters and Setters
 }

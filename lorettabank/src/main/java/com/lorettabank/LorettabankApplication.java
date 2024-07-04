dbb3b657c84a5edd64f7c90fed53d3aad3418c48
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 12:07:25 +0200

    Update and rename LorrettabankApplication.java to LorettabankApplication.java

--- lorettabank/src/main/java/com/lorettabank/LorettabankApplication.java
+++ lorettabank/src/main/java/com/lorettabank/LorettabankApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LorrettabankApplication {
+public class LorettaBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LorrettabankApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LorettaBankApplication.class, args);
+    }
 }

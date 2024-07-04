c1fe24debbd54baa1ef454a0b8412f5873c62baf
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-05 00:35:33 +0200

    Update AccountRepository.java

--- lorettabank/src/main/java/com/lorettabank/repository/AccountRepository.java
+++ lorettabank/src/main/java/com/lorettabank/repository/AccountRepository.java
@@ -3,5 +3,8 @@ package com.lorettabank.repository;
 import com.lorettabank.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUserId(Long userId);
 }

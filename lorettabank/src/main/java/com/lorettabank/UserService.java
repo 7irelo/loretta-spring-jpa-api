274f2be04ea8d3f2de66f29c5466a5673437c88a
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 12:24:00 +0200

    Create UserService.java

---
+++ lorettabank/src/main/java/com/lorettabank/UserService.java
@@ -0,0 +1,26 @@
+package com.lorettabank.service;
+
+import com.lorettabank.model.User;
+import com.lorettabank.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}

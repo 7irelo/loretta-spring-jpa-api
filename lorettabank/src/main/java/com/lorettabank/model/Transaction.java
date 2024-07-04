f3cc176438f1d404d9a273560eac3dd8ce17a0a9
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-05 00:34:25 +0200

    Update Transaction.java

--- lorettabank/src/main/java/com/lorettabank/model/Transaction.java
+++ lorettabank/src/main/java/com/lorettabank/model/Transaction.java
@@ -1,16 +1,27 @@
 package com.lorettabank.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Transaction {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String type;
-    private Double amount;
+
     @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
     private Account account;
-    // getters and setters
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date timestamp = new Date();
+
+    // Getters and Setters
 }

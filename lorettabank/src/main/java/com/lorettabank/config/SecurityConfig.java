b3f46f426aea55a0d0612ba93cc58c55b57dcdd0
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-04 12:35:55 +0200

    Create SecurityConfig.java

---
+++ lorettabank/src/main/java/com/lorettabank/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.lorettabank.config;
+
+import com.lorettabank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation

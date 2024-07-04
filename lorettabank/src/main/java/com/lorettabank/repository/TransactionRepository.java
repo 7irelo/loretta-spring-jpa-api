e2e810966c88b41a298e41a21a4abb1cff6d2da1
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-05 00:36:06 +0200

    Update TransactionRepository.java

--- lorettabank/src/main/java/com/lorettabank/repository/TransactionRepository.java
+++ lorettabank/src/main/java/com/lorettabank/repository/TransactionRepository.java
@@ -3,5 +3,8 @@ package com.lorettabank.repository;
 import com.lorettabank.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountId(Long accountId);
 }
